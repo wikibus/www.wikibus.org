e485d6b744c1d3290cbdf7dd0dfe1ac1f97afad4
tpluscode <tpluscode@users.noreply.github.com>
2015-08-16 19:06:32 +0200

    import openmind theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,61 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    del = require('del'),
+    gulpIgnore = require('gulp-ignore'),
+    historyApiFallback = require('connect-history-api-fallback'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less');
 
 gulp.task('default', ['clean'], function() {
     gulp.start('build');
 });
 
-gulp.task('build', [ 'bower-files', 'html', 'connect', 'watch']);
+gulp.task('build', [ 'bower-files', 'html', 'css', 'connect', 'watch']);
 
 gulp.task('bower-files', function() {
     gulp.src('bower_components/**/*.*', { base: 'bower_components' })
         .pipe(gulpIgnore.exclude("*.map"))
-        .pipe(gulp.dest('dist/bower_components'));
+        .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('css', function () {
+    gulp.src('src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'));
+
+    gulp.src('src/styles/openmind.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('html', function() {
-    gulp.src('src/**/*.html')
+    gulp.src([
+            'src/**/*.html',
+            'src/images/**/*.*'
+        ], {
+            base: 'src'
+        })
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('connect', function() {
     connect.server({
         root: 'dist',
-        port: 9700,
-        middleware: function(connect, opt) {
-            return [
-                historyApiFallback
-            ];
-        }
+        port: 9700
     });
 });
 
 gulp.task('watch', function() {
     gulp.watch(['src/**/*.html'], ['html', 'reload']);
     gulp.watch(['src/**/*.js'], ['js', 'reload']);
-    gulp.watch(['src/**/*.css'], ['css', 'reload']);
+    gulp.watch(['src/**/*.scss'], ['sass', 'reload']);
+    gulp.watch(['src/**/*.less'], ['sass', 'reload']);
 });
 
 gulp.task('clean', function(cb) {
     del('dist', cb);
+});
+
+gulp.task('reload', function() {
+    connect.reload();
 });
