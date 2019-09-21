a14a5792fe42385e35a10222584f4aa4abd3f71b
tpluscode <tpluscode@users.noreply.github.com>
2019-09-21 17:26:23 +0200

    fix: improve library the background

--- src/views/library/index.ts
+++ src/views/library/index.ts
@@ -28,7 +28,7 @@ ViewTemplates.default.when
 
     return html`
       <canvas-emphasis-title
-        background-image="https://cdn.pixabay.com/photo/2018/06/01/13/54/books-3446451_1280.jpg"
+        background-image="https://pixabay.com/get/55e4d1454e57ad14f6d1867dda6d49214b6ac3e45657784972267adc93/books-3446451_1920.jpg"
         heading="wikibus.org library"
         lead="Here you can explore the private collection of books and other physical and electronic media about public transport"
       ></canvas-emphasis-title>
