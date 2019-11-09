b020a08dd007dbe0dd25e6c03d33ccf508c7bdc1
tpluscode <tpluscode@users.noreply.github.com>
2019-11-09 10:18:54 +0100

    fix: don't render empty widget when there is no operation

--- src/views/portfolio-gallery/sidebar.ts
+++ src/views/portfolio-gallery/sidebar.ts
@@ -3,14 +3,13 @@ import { html } from 'lit-html'
 import { Vocab } from 'alcaeus'
 import { Collection } from 'alcaeus/types/Resources'
 import { operationSelector } from '../scopes'
+import { widget } from '../_canvas/widget'
 
 ViewTemplates.default.when.scopeMatches('collection-sidebar').renders(
   (resource: Collection, next) => html`
     <div class="sidebar nobottommargin">
       <div class="sidebar-widgets-wrap">
-        <div class="widget clearfix">
-          ${next(resource, operationSelector)}
-        </div>
+        ${widget(next(resource, operationSelector))}
         <div class="widget quick-contact-widget form-widget clearfix">
           <h4>Search</h4>
           <url-template-form
