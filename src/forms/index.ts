97d9c425ca04a540c804bbbcacf12191bfbe0c47
tpluscode <tpluscode@users.noreply.github.com>
2020-05-07 19:10:18 +0200

    feat: dropdown component to filter by users

--- src/forms/index.ts
+++ src/forms/index.ts
@@ -2,10 +2,24 @@ import { FieldTemplates } from '@lit-any/forms'
 import { checkbox } from '@lit-any/forms/components'
 import { FieldContract } from '@lit-any/forms/lib/formContract'
 import { dcterms, schema, xsd } from '@tpluscode/rdf-ns-builders'
-import { html } from 'lit-html'
+import { html, directive, PropertyPart, Part } from 'lit-html'
 import { until } from 'lit-html/directives/until'
+import { Hydra } from 'alcaeus'
+import { Collection } from 'alcaeus/types/Resources'
 import * as CanvasComponents from './CanvasComponents'
 import { typeMatches } from './matchers'
+
+const stateMap = new WeakMap<Part, boolean>()
+
+const resolveOnce = directive((initialPromise: () => Promise<any>) => async (part: Part) => {
+  const done = stateMap.has(part)
+  if (!done && part instanceof PropertyPart) {
+    const resolved = await initialPromise()
+    stateMap.set(part, true)
+    part.setValue(resolved)
+    part.commit()
+  }
+})
 
 FieldTemplates.default.useComponents(CanvasComponents)
 
@@ -22,6 +36,52 @@ FieldTemplates.default.when
         @change="${(e: any) => set(e.target.value)}"
         style="width: 100%"
       ></vaadin-date-picker>
+    `
+  })
+
+FieldTemplates.default.when
+  .fieldMatches(field => field.property === schema.contributor.value)
+  .renders((f, id, v: any, set) => {
+    import('@vaadin/vaadin-combo-box/vaadin-combo-box.js')
+
+    async function dataProvider(params: any, callback: any) {
+      const res = await Hydra.loadResource('https://users.wikibus.org/contributors')
+
+      const collection = res.root as Collection | null
+      if (collection) {
+        callback(
+          collection.members.map(m => ({
+            id: m.id,
+            label: m[schema.name.value],
+          })),
+          collection.totalItems,
+        )
+      }
+    }
+
+    async function loadInitialUser() {
+      if (v) {
+        const { root } = await Hydra.loadResource(v)
+        if (root) {
+          return {
+            id: root.id,
+            label: root[schema.name.value],
+          }
+        }
+      }
+
+      return undefined
+    }
+
+    return html`
+      <vaadin-combo-box
+        .dataProvider="${dataProvider}"
+        item-id-path="id"
+        .selectedItem="${resolveOnce(loadInitialUser)}"
+        @selected-item-changed="${(e: any) => {
+          set(e.detail.value.id)
+        }}"
+      ></vaadin-combo-box>
     `
   })
 
